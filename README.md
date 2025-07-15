5875a5c2598087e97d019173704a0015ca17b2bf
Rafael Olegário <rafaelolegario2008@gmail.com>
2025-07-15 13:39:28 -0300

    Novo visual; remoção da tela separada de criação e do timer da task

--- README.md
+++ README.md
@@ -1,10 +1,19 @@
 # LightTasks
-A simple software to register daily tasks, with an RPG character customization and skill upgrade system.
-I'm using CustomTkinter to build the GUI. The software is currently in development. Right now, you can only register tasks and set the total time required for each one, with a limit of 7 tasks.
+A software to register daily tasks.
+I'm using CustomTkinter to build the GUI. The software is currently in development.
+
+## Updates:
+- The time of the tasks are removed.
+- Now you can creat how much tasks you want.
+- Now the tasks have a checkbox.
+- Now the create task menu in in the main menu, to more minimalism and optimization.
+- Now you can delete all your tasks.
+- New visual in main menu.
+- New ProgressBar that'll be growing according the tasks are completed.
+- New Configuration Menu.
 
 ## Planned features:
 - Allow an unlimited number of tasks
 - Enable users to delete individual tasks or all tasks
 - Add a progress bar that fills as tasks are completed
-- Create a menu with RPG character customization, where users can unlock options by leveling up
 - More improvements, bug fixes, and new mechanics
--- main.py
+++ main.py
@@ -11,78 +11,83 @@ janela.iconbitmap('iconapp.ico')
 #Variáveis
 tasks_dict = {}
 tasks_list = []
-check = 0
-n1 = 0
-n2 = 0.1
 #Funções)
-def janelacreatetask():
-    def createtask():
-        global check, n1, n2
-        if n2 == 0.7:
-            CTkMessagebox(title='ERRO', message='Limite de Tasks atingidas, limpe o cachê para continuar.', icon='cancel', option_1='Ok')
-        else:
-            if check != 0:
-                n2+=0.1
-            areatasks = CTkFrame(janela, width=400, height=50, border_width=2, border_color='white')
-            areatasks.place(relx=n1, rely=n2)
-            areatasks.pack_propagate(False)
+def createtask():
+    global nome_task, areatasks
+    nome_da_task = nome_task.get()
+    if not nome_da_task:
+        CTkMessagebox(janela, message='Input a name on task.', icon='cancel', option_1='Ok', title='ERROR', fg_color='black', border_color='white', border_width=2, bg_color='black', button_color='white', button_text_color='black', button_hover_color='#b5b5b5', corner_radius=30, cancel_button='white', button_width=50)
+    else:
+        #frame principal
+        areatasks = CTkFrame(scroll, width=400, height=50, border_width=2, border_color='white', fg_color='transparent')
+        areatasks.pack(pady=5)
+        areatasks.pack_propagate(False)
 
-            texto_nome = tasks_dict['Nome'] = nome.get()
-            tasks_dict['Tempo'] = tempo.get()
-            if len(texto_nome) > 20:
-                texto_nome = texto_nome[:20]+'...'
-            nome_icon = CTkImage(dark_image=Image.open('./icontask.png'), size=(20,20))
-            nome_label = CTkLabel(areatasks, text=texto_nome, font=('comic sans ms', 20), image=nome_icon, compound='left')
-            nome_label.place(relx=0.02, rely=0.18)
+        #nome da task
+        texto_nome = tasks_dict['Nome'] = nome_da_task
+        if len(texto_nome) > 25:
+            texto_nome = texto_nome[:25]+'...'
+        nome_icon = CTkImage(dark_image=Image.open('./icontask.png'), size=(20,20))
+        nome_label = CTkLabel(areatasks, text=texto_nome, font=('comic sans ms', 20), image=nome_icon, compound='left')
+        nome_label.place(relx=0.02, rely=0.18)
+        tasks_dict['Label'] = nome_label
 
-            tempo_icon = CTkImage(dark_image=Image.open('./icontime.png'), size=(25,25))
-            tempo_label = CTkLabel(areatasks, text=tasks_dict['Tempo']+'H', font=('comic sans ms', 20), wraplength=300, image=tempo_icon,
-                                   compound='left')
-            tempo_label.place(relx=0.85,rely=0.18)
+        #checkbox da task
+        check_box = CTkCheckBox(areatasks, text='', onvalue='on', offvalue='off', width=30, height=30, fg_color='green')
+        check_box.place(relx=0.90, rely=0.20)
 
-            tasks_list.append(tasks_dict.copy())
-            tasks_dict.clear()
-            check+=1
-            print(n2)
-
-    janela_task = CTkToplevel(janela, fg_color='black')
-    janela_task.geometry('400x200')
-    janela_task.title('Create Task')
-    janela_task.resizable(width=False, height=False)
-    janela_task.iconbitmap('iconapp.ico')
-
-    ceu_imagem = CTkImage(dark_image=Image.open('./fundo.jpg'), size=(400, 500))
-    ceu_imagemlabel = CTkLabel(janela_task, width=400, height=200, image=ceu_imagem, text='')
-    ceu_imagemlabel.place(relx=0.0, rely=0.0)
-
-    nome = CTkEntry(janela_task, width=200, placeholder_text='Nome da Task...', fg_color='black', bg_color='black', border_color='black')
-    nome.pack(pady=20)
-
-    tempo = CTkEntry(janela_task, width=200, placeholder_text='Tempo Total em Horas...', fg_color='black', bg_color='black', border_color='black')
-    tempo.pack(pady=10)
-
-    botao_ok = CTkButton(janela_task, width=50, height=20, text='Create', fg_color='#90ee90', font=('comic sans ms', 20),
-                         hover_color='#006400', border_color='#90ee90', text_color='black', command=createtask, border_width=0,
-                         corner_radius=0)
-    botao_ok.pack(pady=20)
-
+        tasks_list.append(tasks_dict.copy())
+        tasks_dict.clear()
+        nome_task.delete(0, END)
+def deletetasks():
+    global areatasks
+    check = CTkMessagebox(janela, message='Você quer realmente deletar todas as suas tasks?', icon='warning', option_1='Yes', option_2='Cancel', title='WARNING', button_width=100)
+    if check.get() == 'Cancel':
+        pass
+        print(tasks_list)
+    elif check.get() == 'Yes':
+        tasks_list.clear()
+        areatasks.destroy()
 
 #Menu Principal
-#Fundo
-ceu_imagem = CTkImage(dark_image=Image.open('./fundo.jpg'), size=(400,500))
-ceu_imagemlabel = CTkLabel(janela, width=200, height=200, image=ceu_imagem, text='')
-ceu_imagemlabel.place(relx=0.0,rely=0.0)
+#Menu de Configurações de baixo
+config_menu = CTkFrame(janela, width=400, height=50, fg_color='black', border_width=2, border_color='white')
+config_menu.pack_propagate(False)
+config_menu.pack(side='bottom')
 
-#Textos
-opcoes = CTkFrame(janela, width=400, height=50, border_width=2, border_color='white')
+#Botão configuração e Delete
+config_image = CTkImage(dark_image=Image.open('./configicon.png'), size=(20,20))
+config_button = CTkButton(config_menu, width=40, height=40, fg_color='white', text='', hover_color='#b5b5b5', image=config_image)
+config_button.pack(side='right',padx=5)
+delete_image = CTkImage(dark_image=Image.open('./lixeiraicon.png'), size=(20,20))
+delete_button = CTkButton(config_menu, width=40, height=40, text='', image=delete_image, fg_color='#990000', hover_color='#3D0000', command=deletetasks)
+delete_button.pack(side='right')
+#Probressbar para as tasks
+progress_bar = CTkProgressBar(config_menu, width=250, height=10, border_color='white', border_width=2, progress_color='#37FD12')
+progress_bar.pack(side='left', padx=10)
+progress_bar.set(0)
+#Scroll Frame
+scroll = CTkScrollableFrame(janela, width=400, height=390, fg_color='black', border_width=2, border_color='white', scrollbar_fg_color='black', scrollbar_button_color='white')
+scroll.pack(side='bottom')
+
+#Menu de Criação no topo
+opcoes = CTkFrame(janela, width=400, height=50, border_width=2, border_color='white', fg_color='black')
 opcoes.place(relx=0,rely=0)
 opcoes.pack_propagate(False)
 
-mytasks_name = CTkLabel(opcoes, text='M y T  a  s  k  s', font=('comic sans ms', 20), fg_color='transparent')
-mytasks_name.place(relx=0.04, rely=0.15)
+#Entry com nome da Task
+nome_task = CTkEntry(opcoes, width=150, placeholder_text='Task name...', fg_color='black', bg_color='black', border_color='white', corner_radius=25, border_width=2, placeholder_text_color='white')
+nome_task.place(relx=0.50, rely=0.20)
+nome_task.bind("<Return>", lambda event: createtask())
+
+#Título
+logo_image = CTkImage(dark_image=Image.open('./iconapp.ico'), size=(30,30))
+mytasks_name = CTkLabel(opcoes, text='LightTasks', font=('comic sans ms', 20), fg_color='transparent', image=logo_image, compound='left')
+mytasks_name.place(relx=0.03, rely=0.15)
+
 
 #Botões
-criartasks = CTkButton(opcoes, text='+', width=30, height=30, fg_color='#90ee90', corner_radius=10, hover_color='#006400', text_color='black',
-                       command=janelacreatetask)
-criartasks.place(relx=0.9, rely=0.15)
+criartasks = CTkButton(opcoes, text='+', width=30, height=30, fg_color='white', corner_radius=10, hover_color='#b5b5b5', text_color='black',
+                       command=createtask)
+criartasks.place(relx=0.9, rely=0.19)
 janela.mainloop()
